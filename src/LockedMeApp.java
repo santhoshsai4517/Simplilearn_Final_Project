3d2ee9ff69b5f64c27719f73b830c57ea4bb366e
santhosh <santhoshsai4517@gmail.com>
2023-06-17 14:33:23 +0530

    Completed the tasks in Sprint 2 and tested the functionality the tasks using junit.

--- src/LockedMeApp.java
+++ src/LockedMeApp.java
@@ -15,22 +15,26 @@ public class LockedMeApp {
     private static void displayWelcomeScreen() {
         System.out.println("=====================================");
         System.out.println("        Welcome to LockedMe.com       ");
-        System.out.println("      Developed by Santhosh      ");
+        System.out.println("        Developed by Santhosh       ");
         System.out.println("=====================================");
     }
 
     private static void showMainMenu() {
         System.out.println("\n[MAIN MENU]");
         System.out.println("1. Retrieve file names in ascending order");
-        System.out.println("2. Close the application");
+        System.out.println("2. Manage files");
+        System.out.println("3. Close the application");
 
-        int choice = getUserChoice(1, 2);
+        int choice = getUserChoice(1, 3);
 
         switch (choice) {
             case 1:
                 retrieveFileNames();
                 break;
             case 2:
+                showFileManagementMenu();
+                break;
+            case 3:
                 closeApplication();
                 break;
         }
@@ -66,12 +70,97 @@ public class LockedMeApp {
         return fileNames;
     }
 
+    private static void showFileManagementMenu() {
+        System.out.println("\n[FILE MANAGEMENT MENU]");
+        System.out.println("1. Add a file to the directory");
+        System.out.println("2. Delete a file from the directory");
+        System.out.println("3. Search for a file in the directory");
+        System.out.println("4. Navigate back to the main menu");
+
+        int choice = getUserChoice(1, 4);
+
+        switch (choice) {
+            case 1:
+                addFile();
+                break;
+            case 2:
+                deleteFile();
+                break;
+            case 3:
+                searchFile();
+                break;
+            case 4:
+                showMainMenu();
+                break;
+        }
+    }
+
+    static void addFile() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter the name of the file to add: ");
+        String fileName = scanner.nextLine();
+
+        File file = new File(DIRECTORY_PATH + File.separator + fileName);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File added successfully.");
+            } else {
+                System.out.println("File already exists in the directory.");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred while adding the file: " + e.getMessage());
+        }
+
+        showFileManagementMenu();
+    }
+
+    static void deleteFile() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter the name of the file to delete: ");
+        String fileName = scanner.nextLine();
+
+        File file = new File(DIRECTORY_PATH + File.separator + fileName);
+        if (file.exists()) {
+            if (file.delete()) {
+                System.out.println("File deleted successfully.");
+            } else {
+                System.out.println("Failed to delete the file.");
+            }
+        } else {
+            System.out.println("File not found.");
+        }
+
+        showFileManagementMenu();
+    }
+
+    static void searchFile() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter the name of the file to search: ");
+        String fileName = scanner.nextLine();
+
+        File directory = new File(DIRECTORY_PATH);
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().equals(fileName)) {
+                    System.out.println("File found: " + file.getAbsolutePath());
+                    showFileManagementMenu();
+                    return;
+                }
+            }
+        }
+
+        System.out.println("File not found.");
+        showFileManagementMenu();
+    }
+
     private static void closeApplication() {
         System.out.println("\nClosing the application...");
         System.exit(0);
     }
 
-    private static int getUserChoice(int minChoice, int maxChoice) {
+    static int getUserChoice(int minChoice, int maxChoice) {
         Scanner scanner = new Scanner(System.in);
         int choice;
 
--- src/LockedMeAppTest.java
+++ src/LockedMeAppTest.java
@@ -1,20 +1,29 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LockedMeAppTest {
+    private static final String TEST_DIRECTORY_PATH = "C:\\Users\\santh\\Desktop\\Simplilearn_projects\\Final_Project\\test"; // Update with the actual test directory path
+    private static final String TEST_FILE_NAME = "testFile.txt";
 
-    private static final String TEST_DIRECTORY_PATH = "C:\\Users\\santh\\Desktop\\Simplilearn_projects\\Final_Project\\test\\"; // Update with the actual test directory path
+    private File testDirectory;
+    private ByteArrayOutputStream outputStream;
 
     @BeforeEach
     void setup() {
+        testDirectory = new File(TEST_DIRECTORY_PATH);
+        testDirectory.mkdir();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         deleteTestDirectory();
         createTestDirectory();
     }
@@ -40,80 +49,141 @@ public class LockedMeAppTest {
         }
     }
 
-      
-    
     @Test
     void testRetrieveFileNames_EmptyDirectory() {
-        // Arrange
-        String expectedOutput = "No files found in the directory.";
+        List<String> expectedFileNames = Arrays.asList();
+        List<String> actualFileNames = LockedMeApp.getFileNamesInAscendingOrder();
 
-        // Act
-        String actualOutput = retrieveFileNames();
-
-        // Assert
-        Assertions.assertEquals(expectedOutput, actualOutput);
+        Assertions.assertEquals(expectedFileNames, actualFileNames);
     }
 
     @Test
     void testRetrieveFileNames_NonEmptyDirectory() {
-        // Arrange
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1.txt");
-        fileNames.add("file2.txt");
-        fileNames.add("file3.txt");
-        prepareTestDirectory(fileNames);
+        createTestFile(TEST_FILE_NAME);
 
-        // Act
-        String actualOutput = retrieveFileNames();
+        List<String> expectedFileNames = Arrays.asList(TEST_FILE_NAME);
+        List<String> actualFileNames = LockedMeApp.getFileNamesInAscendingOrder();
 
-        // Assert
-        Assertions.assertTrue(actualOutput.contains("file1.txt"));
-        Assertions.assertTrue(actualOutput.contains("file2.txt"));
-        Assertions.assertTrue(actualOutput.contains("file3.txt"));
+        Assertions.assertEquals(expectedFileNames, actualFileNames);
     }
 
     @Test
-    void testRetrieveFileNames_InvalidDirectory() {
-        // Arrange
-        String expectedOutput = "No files found in the directory.";
-        prepareTestDirectory(null);
+    void testAddFile_FileAddedSuccessfully() {
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
 
-        // Act
-        String actualOutput = retrieveFileNames();
+        LockedMeApp.addFile();
 
-        // Assert
+        String expectedOutput = "File added successfully.";
+        String actualOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+        Assertions.assertTrue(fileExists(TEST_FILE_NAME));
+    }
+
+    @Test
+    void testAddFile_FileAlreadyExists() {
+        createTestFile(TEST_FILE_NAME);
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
+
+        LockedMeApp.addFile();
+
+        String expectedOutput = "File already exists in the directory.";
+        String actualOutput = outputStream.toString().trim();
+
         Assertions.assertEquals(expectedOutput, actualOutput);
     }
-    
-    
-    private void prepareTestDirectory(List<String> fileNames) {
-        File directory = new File(TEST_DIRECTORY_PATH);
-        directory.mkdirs();
 
-        if (fileNames != null) {
-            for (String fileName : fileNames) {
-                File file = new File(directory, fileName);
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    @Test
+    void testDeleteFile_FileDeletedSuccessfully() {
+        createTestFile(TEST_FILE_NAME);
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
+
+        LockedMeApp.deleteFile();
+
+        String expectedOutput = "File deleted successfully.";
+        String actualOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+        Assertions.assertFalse(fileExists(TEST_FILE_NAME));
+    }
+
+    @Test
+    void testDeleteFile_FileNotFound() {
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
+
+        LockedMeApp.deleteFile();
+
+        String expectedOutput = "File not found.";
+        String actualOutput = outputStream.toString().trim();
+
+        Assertions.assertTrue(actualOutput.contains(expectedOutput));
+    }
+
+    @Test
+    void testSearchFile_FileFound() {
+        createTestFile(TEST_FILE_NAME);
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
+
+        LockedMeApp.searchFile();
+
+        String expectedOutput = "File found: " + TEST_DIRECTORY_PATH + File.separator + TEST_FILE_NAME;
+        String actualOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void testSearchFile_FileNotFound() {
+        provideUserInput(TEST_FILE_NAME + System.lineSeparator());
+
+        LockedMeApp.searchFile();
+
+        String expectedOutput = "File not found.";
+        String actualOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void testGetUserChoice_ValidChoice() {
+        provideUserInput("2" + System.lineSeparator());
+
+        int minChoice = 1;
+        int maxChoice = 3;
+        int expectedChoice = 2;
+        int actualChoice = LockedMeApp.getUserChoice(minChoice, maxChoice);
+
+        Assertions.assertEquals(expectedChoice, actualChoice);
+    }
+
+    @Test
+    void testGetUserChoice_InvalidChoiceThenValidChoice() {
+        provideUserInput("invalid" + System.lineSeparator() + "2" + System.lineSeparator());
+
+        int minChoice = 1;
+        int maxChoice = 3;
+        int expectedChoice = 2;
+        int actualChoice = LockedMeApp.getUserChoice(minChoice, maxChoice);
+
+        Assertions.assertEquals(expectedChoice, actualChoice);
+    }
+
+    private void createTestFile(String fileName) {
+        File file = new File(testDirectory, fileName);
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private String retrieveFileNames() {
-        List<String> fileNames = LockedMeApp.getFileNamesInAscendingOrder();
-        StringBuilder output = new StringBuilder();
+    private boolean fileExists(String fileName) {
+        File file = new File(testDirectory, fileName);
+        return file.exists();
+    }
 
-        if (fileNames.isEmpty()) {
-            output.append("No files found in the directory.");
-        } else {
-            for (String fileName : fileNames) {
-                output.append(fileName).append("\n");
-            }
-        }
-
-        return output.toString();
+    private void provideUserInput(String userInput) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
     }
 }
